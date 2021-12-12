952616ebfa405b882ec773410f4aecaa8cf42f12
Dmitriy <dima.ermolovich04@gmail.com>
2021-12-12 17:13:20 +0300

    add new test

--- src/test/java/CellTest.java
+++ src/test/java/CellTest.java
@@ -3,15 +3,24 @@ import org.junit.Test;
 public class CellTest {
     @Test
     public void testOpen() {
+        var cell = new Cell(null);
+        assert true;
     }
-
+    @Test
     public void testMine() {
+        var Cell = new Cell(null);
+        Cell.mine();
+        assert Cell.isMined();
     }
-
+    @Test
     public void testSetCountBomb() {
+        var cell = new Cell(null);
+        assert true;
     }
-
+    @Test
     public void testGetCountBomb() {
+        var cell = new Cell(null);
+        assert true;
     }
     @Test
     public void testIsNotOpen() {
@@ -26,17 +35,25 @@ public class CellTest {
         cell.mine();
         assert cell.isMined();
     }
-
+    @Test
     public void testInverseFlag() {
+        var cell = new Cell(null);
+        assert true;
     }
-
+    @Test
     public void testPaintBomb() {
+        var cell = new Cell(null);
+        assert true;
     }
-
+    @Test
     public void testPaintString() {
+        var cell = new Cell(null);
+        assert true;
     }
-
+    @Test
     public void testPaint() {
+        var cell = new Cell(null);
+        assert true;
     }
 
 }
